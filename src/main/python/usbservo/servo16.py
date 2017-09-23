7c2aa972ee0e9e308abc06c2ac5c984b022fcc11
aadebuger <zhvxxh@gmail.com>
2017-09-23 14:05:12 +0800

    comand

--- src/main/python/usbservo/servo16.py
+++ src/main/python/usbservo/servo16.py
@@ -26,24 +26,50 @@ import time
 usbport = '/dev/tty.SLAB_USBtoUART'
 
 # Set up serial baud rate
+# big controller
+#ser = serial.Serial(usbport, 115200, bytesize=8,stopbits=1,parity=serial.PARITY_NONE)
+
 ser = serial.Serial(usbport, 9600, bytesize=8,stopbits=1,parity=serial.PARITY_NONE)
+
 
 
 def command(servo, angle):
         ser.write(chr(255))
         ser.write(chr(0x1))
         ser.write(chr(0x0))
-        ser.write(chr(0x1))
+        ser.write(chr(0x0a))
         ser.write(chr(0x0)) 
 def command1(servo, angle):
         ser.write(chr(255))
         ser.write(chr(0x2))
         ser.write(chr(0x0))
+        ser.write(chr(0xf4))
+        ser.write(chr(0x01))
+def command11(servo, angle):
+        ser.write(chr(255))
+        ser.write(chr(0x2))
+        ser.write(chr(0x0))
         ser.write(chr(0xdc))
-        ser.write(chr(0x05)) 
+        ser.write(chr(0x05))
+def command12(servo, angle):
+        ser.write(chr(255))
+        ser.write(chr(0x2))
+        ser.write(chr(servo))
+        ser.write(chr(angle%255))
+        ser.write(chr(angle/255)) 
+def command13(servo, angle):
+        angle1= int(2000.0/180.0*angle + 500)
+        print("angle1",angle1)
+
+        ser.write(chr(255))
+        ser.write(chr(0x2))
+        ser.write(chr(servo))
+        ser.write(chr(angle1%255))
+        ser.write(chr(angle1/255)) 
+
 def command2(servo, angle):
         
-        ser.write("#10P10T1000\r\n".encode())
+        ser.write("#10P2500T1000\r\n".encode())
         time.sleep(5)
 def move(servo, angle):
     '''Moves the specified servo to the supplied angle.
@@ -65,5 +91,7 @@ def move(servo, angle):
         print "Servo angle must be an integer between 0 and 180.\n"
 
 if __name__ == '__main__':
-#        command(1,10)
-        command2(1,10)
+        command(1,10)
+        command11(0,500)
+#        command2(1,10)
+        time.sleep(5)
